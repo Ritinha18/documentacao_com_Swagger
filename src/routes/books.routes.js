320b764938f46446f109ce02e29fa1cb4dc7f5a4
Ritinha18 <rita.7982@aluno.pr.senac.br>
2024-03-12 08:41:34 -0300

    Alterações feitas novamente

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -2,11 +2,12 @@ const {Router} = require("express")
 const BookController = require("../controllers/BookController")
 const userRoutes = require("./users.routes")
 const checkBooksExists = require("../middlewares/checkBooksExists")
+const checkUsersExists = require("../middlewares/checkUsersExists")
 
 const bookRoutes = Router()
 const bookController = new BookController()
 
-bookRoutes.post("/books/:user_id", bookController.createBook )
+bookRoutes.post("/books/:user_id", checkUsersExists, bookController.createBook )
 
 bookRoutes.get("/books", bookController.listBook)
 
