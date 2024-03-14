af1e46baa0607c0c168ed5f39d19486e109026ae
Ritinha18 <rita.7982@aluno.pr.senac.br>
2024-03-14 11:42:20 -0300

    Swagger.json

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const express = require("express")
 const routes = require("./src/routes")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./src/swagger.json');
+
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
